d1f5dc42af43d6a32315eead812581d85ce8401d
Benedikt Reiser <burnedikt@users.noreply.github.com>
2021-06-06 02:14:00 +0200

    chore: increase compatibililty with oauthlib versions

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     keywords=['oauth', 'oauthlib', 'grant'],
     packages=find_packages(exclude=['docs', 'tests', 'tests.*']),
     install_requires=[
-        'oauthlib==1.1.2'
+        'oauthlib>=1.1.2'
     ],
     test_suite='nose.collector',
     tests_require=['nose'],
